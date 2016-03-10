a0507c1fcce960c5ecbc1425046192aa8b61131a
csielee <lee097019@gmail.com>
2016-03-10 13:27:05 +0800

    減少struct大小 136->32

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -5,20 +5,39 @@
 
 /* TODO: After modifying the original version, uncomment the following
  * line to set OPT properly */
-// #define OPT 1
-typedef struct __PHONE_BOOK_ENTRY {
-    char lastName[MAX_LAST_NAME_SIZE];
-    char firstName[16];
-    char email[16];
-    char phone[10];
-    char cell[10];
-    char addr1[16];
-    char addr2[16];
-    char city[16];
-    char state[2];
-    char zip[5];
-    struct __PHONE_BOOK_ENTRY *pNext;
-} entry;
+#define OPT 1
+ struct __PHONE_BOOK_DATA {
+
+        char firstName[16];
+
+        char email[16];
+
+        char phone[10];
+
+        char cell[10];
+
+        char addr1[16];
+
+        char addr2[16];
+
+        char city[16];
+
+        char state[2];
+
+        char zip[5];
+
+    };
+
+
+    typedef struct __PHONE_BOOK_ENTRY {
+
+        char lastName[MAX_LAST_NAME_SIZE];
+
+        struct __PHONE_BOOK_DATA *pData;
+
+        struct __PHONE_BOOK_ENTRY *pNext;
+
+    } entry;
 
 entry *findName(char lastname[], entry *pHead);
 entry *append(char lastName[], entry *e);
